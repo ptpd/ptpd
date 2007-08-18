b22a2201b2e1a36d19cc377501604bfd72864b6a
Kendall Correll <kendall_c@users.sourceforge.net>
2007-08-18 18:50:42 +0000

    fix 'Possible error in Delay Request time value clearing'

--- src/protocol.c
+++ src/protocol.c
@@ -253,8 +253,10 @@ void toState(UInteger8 state, RunTimeOpts *rtOpts, PtpClock *ptpClock)
     DBG("Q = %d, R = %d\n", ptpClock->Q, ptpClock->R);
     
     ptpClock->waitingForFollow = FALSE;
-    ptpClock->delay_req_send_time.seconds = ptpClock->delay_req_receive_time.seconds = 0;
-    ptpClock->delay_req_send_time.nanoseconds = ptpClock->delay_req_receive_time.nanoseconds = 0;
+    ptpClock->delay_req_send_time.seconds = 0;
+    ptpClock->delay_req_send_time.nanoseconds = 0;
+    ptpClock->delay_req_receive_time.seconds = 0;
+    ptpClock->delay_req_receive_time.nanoseconds = 0;
     
     timerStart(SYNC_RECEIPT_TIMER, PTP_SYNC_RECEIPT_TIMEOUT(ptpClock->sync_interval), ptpClock->itimer);
     
@@ -582,8 +584,10 @@ void handleDelayReq(MsgHeader *header, Octet *msgIbuf, ssize_t length, TimeInter
         updateDelay(&ptpClock->delay_req_send_time, &ptpClock->delay_req_receive_time,
           &ptpClock->owd_filt, rtOpts, ptpClock);
         
-        ptpClock->delay_req_receive_time.seconds = ptpClock->delay_req_receive_time.seconds = 0;
-        ptpClock->delay_req_receive_time.nanoseconds = ptpClock->delay_req_receive_time.nanoseconds = 0;
+        ptpClock->delay_req_send_time.seconds = 0;
+        ptpClock->delay_req_send_time.nanoseconds = 0;
+        ptpClock->delay_req_receive_time.seconds = 0;
+        ptpClock->delay_req_receive_time.nanoseconds = 0;
       }
     }
     break;
@@ -635,8 +639,10 @@ void handleDelayResp(MsgHeader *header, Octet *msgIbuf, ssize_t length, Boolean 
         updateDelay(&ptpClock->delay_req_send_time, &ptpClock->delay_req_receive_time,
           &ptpClock->owd_filt, rtOpts, ptpClock);
         
-        ptpClock->delay_req_receive_time.seconds = ptpClock->delay_req_receive_time.seconds = 0;
-        ptpClock->delay_req_receive_time.nanoseconds = ptpClock->delay_req_receive_time.nanoseconds = 0;
+        ptpClock->delay_req_send_time.seconds = 0;
+        ptpClock->delay_req_send_time.nanoseconds = 0;
+        ptpClock->delay_req_receive_time.seconds = 0;
+        ptpClock->delay_req_receive_time.nanoseconds = 0;
       }
     }
     else
