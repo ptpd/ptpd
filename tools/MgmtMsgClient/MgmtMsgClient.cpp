35df1deffc83d22c50419ee120568bd9e5f4d299
Kleinschmidt Tomasz <tomekktcz@users.sourceforge.net>
2012-06-07 00:48:33 +0000

    Main class file created

---
+++ tools/MgmtMsgClient/MgmtMsgClient.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   MgmtMsgClient.cpp
+ * Author: tomek
+ *
+ * Created on 7 czerwiec 2012, 02:06
+ */
+
+/*! \file
+ * \brief Main file for the PTPd Management Message Client
+ *
+ * Used as a glue for all of the client classes
+ */
+
+#include <cstdlib>
+#include <stdio.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
