003fbb1e614284558248a60d09cc5707504876ad
Himanshu Singh <himan92@users.sourceforge.net>
2012-08-09 18:51:29 +0000

    Code Cleaning

--- tools/ptpmanager/network.c
+++ tools/ptpmanager/network.c
@@ -79,11 +79,6 @@ netSendGeneral(Octet * buf, UInteger16 length, char *ip)
 		printf("\nAddress family is not supproted by this function\n");
 		return (0);
 	}
-	int i;
-	printf("\n");
-	for(i=0;i<100;i++)
-	printf("     %d--%x          ",i,(int)*((buf)+i));
-
 	ret = sendto(netPath->generalSock, buf, out_length, 0, 
 			(struct sockaddr *)&addr, sizeof(struct sockaddr_in));
 	printf("\n\n%d\n\n",ret);
