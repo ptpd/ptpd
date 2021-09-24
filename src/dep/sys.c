ffd2e7f5dc71bef296f4c1a009e87c40f1ef66ce
Chris Johns <chrisj@rtems.org>
2021-09-24 11:02:34 +0200

    sys: Add malloc definitions for rtems

--- src/dep/sys.c
+++ src/dep/sys.c
@@ -2668,3 +2668,19 @@ int setCpuAffinity(int cpu) {
 return -1;
 
 }
+
+#if __rtems__
+void *malloc ();
+
+/* Allocate an N-byte block of memory from the heap.
+   If N is zero, allocate a 1-byte block.  */
+
+void *
+rpl_malloc (size_t n)
+{
+  if (n == 0)
+    n = 1;
+  return malloc (n);
+}
+
+#endif /* __rtems__ */
